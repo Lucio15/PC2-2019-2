ea5145bd814387781ed12b2132f02e1464ea2121
Alexander Zevallos <alexanderzevallos2111@gmail.com>
2019-11-20 08:01:59 -0500

    i3

--- p4.py
+++ p4.py
@@ -1,5 +1,7 @@
 import random
 
+pares =  0
+
 N = int(input("Ingrese el número N : "))
 while N < 2 or N > 5 :
     print("Error numero ingresado es incorrecto")
@@ -16,3 +18,12 @@ for i in range (N):
     for j in range (N):
         print (matriz[i][j], end=" ")
     print("")
+
+
+
+for i in range (N):
+    for j in range (N):
+        if matriz [i][j] % 2 == 0 :
+            pares = pares + 1
+
+print("La cantidad de números pares en la matriz es : ",pares)
